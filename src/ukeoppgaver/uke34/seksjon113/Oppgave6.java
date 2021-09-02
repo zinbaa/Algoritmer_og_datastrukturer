1670260e757b4b04c0273b91acf6246c99ef03d7
zinbaa <oslo+zinbaa@users.noreply.github.com>
2021-09-02 16:30:49 +0200

    Laget en metode som regner ut n!

--- src/ukeoppgaver/uke34/seksjon113/Oppgave6.java
+++ src/ukeoppgaver/uke34/seksjon113/Oppgave6.java
@@ -1,4 +1,22 @@
 package ukeoppgaver.uke34.seksjon113;
 
 public class Oppgave6 {
+
+    public static void main(String[] args) {
+
+        System.out.println(fak(0));
+    }
+
+    public static long fak(int n){
+
+        if (n < 0 ) { //vi tar ikke inn negative tall
+            throw new IllegalArgumentException("n < 0, ikke gyldig.");
+        }
+            long fak = 1; //hvis n = 0 eller n = 1 utfores ingen multiplikasjoner
+
+            for (int i=2; i<=n; i++){ //hvis n > 1 utføres det n-1 multiplikasjoner
+                fak*=i; // fak = fak * i;
+            }
+        return fak;
+    }
 }
