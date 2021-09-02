aa4e99d483fe7ac12f744eaba078a157d4b0641b
zinbaa <oslo+zinbaa@users.noreply.github.com>
2021-09-02 20:08:20 +0200

    oppgave for seksjon 115 og seksjon 1110. Oppgave for seksjon1110 er ufullstendig, enda ikke ferdig.

---
+++ src/ukeoppgaver/uke34/seksjon1110/Oppgave_1.java
@@ -0,0 +1,72 @@
+package ukeoppgaver.uke34.seksjon1110;
+
+public class Oppgave_1 {
+
+    public static void main(String[] args) {
+        int n = 100_000, antall = 2_000; // tabellstørrelse og gjentagelser
+        long tid = 0;                    // for tidsmåling
+        //int a[] = randPerm(n);           // en permutasjon av 1, . .  n
+
+    }
+
+    public static int kostnader(int[] a)  // legges i class Program
+    {
+        int m = 0;
+        for (int i = 1; i < a.length; i++) {}  // en tom blokk
+        return m;
+    }
+
+    public static int maks1(int[] a)  // a er en heltallstabell
+    {
+        if (a.length < 1)
+            throw new java.util.NoSuchElementException("Tabellen a er tom!");
+
+        int m = 0;  // indeks til foreløpig største verdi (m for maks)
+
+        for (int i = 1; i < a.length; i++) // obs: starter med i = 1
+        {
+            if (a[i] > a[m]) m = i;  // indeksen oppdateres
+        }
+
+        return m;  // returnerer indeksen/posisjonen til største verdi
+
+    } // maks
+
+    public static int maks2(int[] a)   // versjon 2 av maks-metoden
+    {
+        int m = 0;               // indeks til største verdi
+        int maksverdi = a[0];    // største verdi
+
+        for (int i = 1; i < a.length; i++) if (a[i] > maksverdi)
+        {
+            maksverdi = a[i];     // største verdi oppdateres
+            m = i;                // indeks til største verdi oppdateres
+        }
+        return m;   // returnerer indeks/posisjonen til største verdi
+
+    } // maks
+
+    public static int maks3(int[] a)  // versjon 3 av maks-metoden
+    {
+        int sist = a.length - 1;       // siste posisjon i tabellen
+        int m = 0;                     // indeks til største verdi
+        int maksverdi = a[0];          // største verdi
+        int temp = a[sist];            // tar vare på siste verdi
+        a[sist] = 0x7fffffff;          // legger tallet 2147483647 sist
+
+        for (int i = 0; ; i++)         // i starter med 0
+            if (a[i] >= maksverdi)       // denne blir sann til slutt
+            {
+                if (i == sist)             // sjekker om vi er ferdige
+                {
+                    a[sist] = temp;          // legger siste verdi tilbake
+                    return temp >= maksverdi ? sist : m;   // er siste størst?
+                }
+                else
+                {
+                    maksverdi = a[i];        // maksverdi oppdateres
+                    m = i;                   // m oppdateres
+                }
+            }
+    } // maks
+}
